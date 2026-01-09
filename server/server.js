c25a4d21dadae06620bf6e2992b44a49715c39c9
Akash Das <akashakki1100@gmail.com>
2026-01-09 23:52:58 +0530

    modify

--- server/server.js
+++ server/server.js
@@ -25,6 +25,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is running on port ${PORT} => http://localhost:${PORT} 🍽️`
+    `Server is running on port ${PORT} => http://localhost:${PORT}`
   );
 });
